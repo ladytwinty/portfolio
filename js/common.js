4ccc721d597e11b314cd93c5a57f59b5160039dd
ladytwinty <ladytwinty@gmail.com>
2017-03-14 12:10:15 +0200

    adit block about

---
+++ js/common.js
@@ -0,0 +1,8 @@
+function heightDetect() { // подгон хедера под размер экрана
+		$(".header").css("height", $(window).height())
+	};
+
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
